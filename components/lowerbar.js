8571d58e27aec54eb9552416bb369fdc7f2bbfb9
Maazasim1 <mazzasimq1@gmail.com>
2022-10-08 00:45:51 +0500

    final

--- components/lowerbar.js
+++ components/lowerbar.js
@@ -77,7 +77,7 @@ export default function Lowerbar() {
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="black" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
           </button>
-          {hide || size.width > 400 ?
+          {hide || size.width > 600 ?
 
             <div className=" w-full md:block md:w-auto cursor-pointer" id="navbar-default">
               <ul className='text-white font-Muli flex flex-col p-4 mt-4 bg-[#eda309] rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-[#eda309]'>
